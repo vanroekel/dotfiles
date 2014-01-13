b3f5594445eb9fa38b95d9f570a67a267f3aa2b8
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2014-01-13 13:26:09 -0700

    Adding tmux configuration file.

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ done
 
 ln -s ${PWD}/vim/.vimrc ~/.vimrc
 ln -s ${PWD}/vim/.vimrc.bundles ~/.vimrc.bundles
+ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
 
 cp ${PWD}/git/.gitconfig ~/.
 git config --global core.excludesfile "${PWD}/git/core_excludes"
