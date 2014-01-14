c294164b8f31c44b18b218eb4754ad5bdef3dd09
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2014-01-14 09:24:37 -0700

    Updating find host for yellowstone

--- scripts/find_host.sh
+++ scripts/find_host.sh
@@ -23,6 +23,10 @@ elif [ "${HOST_CHECK}" == "ml" ]; then
     source ${SCRIPT_DIR}/add_modules.sh
     module load friendly-testing
     module load python-epd/7.3.2 cmake/2.8.8 
+elif [ "${HOST_CHECK}" == "yslogin" ]; then
+	export LIBEVENT=/glade/u/home/douglasj/libevent/2.1.3
+	export LD_LIBRARY_PATH="${LIBEVENT}/lib:${LD_LIBRARY_PATH}"
+	export PATH="${TMUX}/bin:${PATH}"
 elif [ "${HOST_CHECK}" == "hopper" ]; then
     module load git
 else # Ubuntu?
