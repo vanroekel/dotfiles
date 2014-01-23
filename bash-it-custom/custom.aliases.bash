b5edf0730762b1da93ab6d9060863bd4ace0244e
Phillip J. Wolfram Jr <pwolfram@mu-fe2.lanl.gov>
2014-01-23 10:03:28 -0700

    Changes for my preferences

--- bash-it-custom/custom.aliases.bash
+++ bash-it-custom/custom.aliases.bash
@@ -1,14 +1,7 @@
 #########################################################################################
 #						Set Aliases for Ease of Use										#
 #########################################################################################
-alias adb-start="adb kill-server && sudo /home/douglasj/AndroidSDK/adt-bundle-linux-x86_64-20130917/sdk/platform-tools/adb start-server"
-alias tunhome="ssh -4fgN -D 23000 home"
-alias tunhome_ip="ssh -4fgN -D 23000 174.56.101.93"
-alias gnuplot="rlwrap -a -c gnuplot"
-alias mountiso="sudo mount -o loop -t iso9660"
 alias ssh='ssh -X'
-alias prepare-wine='echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope'
-alias mpas_tcs='svn co https://svn-mpas-model.cgd.ucar.edu/branches/ocean_projects/ocean_test_cases_staging/ocean'
 alias ps2pdf='ps2pdf13 -dPDFSETTINGS=/prepress -dSubsetFonts=true -dEmbedAllFonts=true -dMaxSubsetPct=100 -dCompatibilityLevel=1.3'
 
 ON_UBUNTU=`uname -a | grep "Ubuntu"`
@@ -32,6 +25,5 @@ alias sshgpfs='ssh wtrw -t ssh gpfst7-fe'
 #########################################################################################
 alias myq_msub='showq -u `whoami`'
 
-alias git_lettuce='git clone git@github.com:douglasjacobsen/mpas-lettuce testing_mpas; cd testing_mpas; git submodule update --init --remote; cd features/ocean; git checkout master; cd ../../../'
 
 alias reset_unity='stop unity-panel-service; start unity-panel-service'
