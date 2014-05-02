ea9f25a282adb8323db27b880ba0b862217b3034
Phillip Wolfram <pwolfram@lanl.gov>
2014-05-01 23:03:38 -0600

    OS X (Darwin) change for set proxy

--- scripts/set_proxy_info.sh
+++ scripts/set_proxy_info.sh
@@ -10,7 +10,7 @@ OS=`uname`
 IO="" # store IP
 case $OS in
 	Linux) IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;;
-	FreeBSD|OpenBSD) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
+	FreeBSD|OpenBSD|Darwin) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
 	SunOS) IP=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{print $2} '` ;;
 	*) IP="Unknown";;
 esac
@@ -28,7 +28,7 @@ done
 
 case $WORKIP in
  	"yes") export http_proxy="http://proxyout.lanl.gov:8080"
- 		export https_proxy="http://proxyout.lanl.gov:8080"
+ 		#export https_proxy="https://proxyout.lanl.gov:8080"
  		export ftp_proxy="http://proxyout.lanl.gov"
  		export no_proxy="*.lanl.gov"
  		#cp ~/.subversion/servers.lanl ~/.subversion/servers
